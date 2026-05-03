6a0e3ae809fe73b7cac1f054d19fa8712a83f295
walid waoua <Walid.Waoua@emsi-edu.ma>
2026-05-03 21:26:20 +0100

    db modif

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,13 +71,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecormerce_prj',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
+     }
 }
 
 
